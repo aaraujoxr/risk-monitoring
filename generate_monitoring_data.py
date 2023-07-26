b51a2c9b2c0840bfe865d57194ec9bea22a8f7e2
Artur de Araujo <aaraujo@voltz.xyz>
2023-07-26 17:29:00 +0200

    Changed so as to save result in arbitrary folder

--- generate_monitoring_data.py
+++ generate_monitoring_data.py
@@ -3,30 +3,34 @@ import os
 import pandas as pd
 
 import gbq_tools
-from sqlQueries.pnlQuery import pnl_query_string
-from sqlQueries.mrQuery import mr_query_string
 
-import monitoring_functions
-import solvency_statistics
+def generate_monitoring_data(path_to_save):
+    import monitoring_functions
 
-timestampstr = datetime.now().strftime("%Y%m%d-%H%M")
+    timestampstr = datetime.now().strftime("%Y%m%d-%H%M")
 
-project = gbq_tools.RiskMonitoringGBQ(version="mr")
-mr_results = project.get_position_history(latest=True)
-mr_results.to_csv(f"monitoring-data/positions-mr-{timestampstr}.csv")
+    project = gbq_tools.RiskMonitoringGBQ(version="mr")
+    mr_results = project.get_position_history(latest=True)
+    mr_results.to_csv(path_to_save + f"positions-mr-{timestampstr}.csv")
 
-pool_specs_df = pd.read_csv("pool-specs.csv", index_col=0)
-pool_specs_df["maturity_date"] = pool_specs_df["maturity_date"].apply(lambda x: datetime.strptime(x,"%m-%d-%Y %H:%M:%S"))
-pool_specs_df["inception_date"] = pool_specs_df["inception_date"].apply(lambda x: datetime.strptime(x,"%m-%d-%Y %H:%M:%S"))
+    pool_specs_df = pd.read_csv("pool-specs.csv", index_col=0)
+    pool_specs_df["maturity_date"] = pool_specs_df["maturity_date"].apply(lambda x: datetime.strptime(x,"%m-%d-%Y %H:%M:%S"))
+    pool_specs_df["inception_date"] = pool_specs_df["inception_date"].apply(lambda x: datetime.strptime(x,"%m-%d-%Y %H:%M:%S"))
 
-full_mr_table = monitoring_functions.complete_mr_table(mr_results, pool_specs_df)
-full_mr_table.to_csv(f"monitoring-data/full-mr-{timestampstr}.csv")
+    full_mr_table = monitoring_functions.complete_mr_table(mr_results, pool_specs_df)
+    full_mr_table.to_csv(path_to_save + f"full-mr-{timestampstr}.csv")
 
-project_id = "risk-monitoring-361911"
-key_path = os.path.expanduser("~/Repositories/Keys/gbq-risk-monitoring-361911-credentials.json")
-project = gbq_tools.GBQProjectWithKey(project_id=project_id, key_path=key_path)
+    project_id = "risk-monitoring-361911"
+    key_path = os.path.expanduser("~/Repositories/Keys/gbq-risk-monitoring-361911-credentials.json")
+    project = gbq_tools.GBQProjectWithKey(project_id=project_id, key_path=key_path)
+    
+    import solvency_statistics
+    from sqlQueries.pnlQuery import pnl_query_string
 
-pnl_results = project.generic_query(pnl_query_string)
-pnl_results.to_csv(f"monitoring-data/positions-{timestampstr}.csv")
+    pnl_results = project.generic_query(pnl_query_string)
+    pnl_results.to_csv(path_to_save + f"positions-{timestampstr}.csv")
 
-solvency_statistics.compute_summary_negative_statistics(timestampstr)
+    solvency_statistics.compute_summary_negative_statistics(timestampstr, path_to_save=path_to_save)
+
+path_to_save = os.path.expanduser("~/Dropbox\Voltz work\Risk\monitoring-data/")
+generate_monitoring_data(path_to_save)
--- solvency_statistics.py
+++ solvency_statistics.py
@@ -1,8 +1,8 @@
-def compute_summary_negative_statistics(identifier):
+def compute_summary_negative_statistics(identifier, path_to_save):
     import pandas as pd
-    from definitions import pool_addresses
+    from definitions import vamm_addresses
 
-    positions_df = pd.read_csv(f"monitoring-data/positions-{identifier}.csv")
+    positions_df = pd.read_csv(path_to_save + f"positions-{identifier}.csv")
     
     positions_df = positions_df.rename(columns={"availableMargin":"Initial balance",
                                               "realizedPnl": "rPnL",
@@ -14,21 +14,21 @@ def compute_summary_negative_statistics(identifier):
     positions_df["Leverage"] = positions_df["variableTokenBalance"]/positions_df["Final balance"]
 
     positions = {chain:
-             {pool: positions_df[positions_df["vammAddress"]==pool_addresses[chain][pool]]
-              for pool in pool_addresses[chain]
+             {pool: positions_df[positions_df["vammAddress"]==vamm_addresses[chain][pool]]
+              for pool in vamm_addresses[chain]
              }
-             for chain in pool_addresses.keys()
+             for chain in vamm_addresses.keys()
             }
     
     multiindex = []
-    for chain in pool_addresses.keys():
-        for pool in pool_addresses[chain].keys():
+    for chain in vamm_addresses.keys():
+        for pool in vamm_addresses[chain].keys():
             multiindex.append((chain, pool))
 
     multiindex = pd.MultiIndex.from_tuples(multiindex)
 
     summary_df = pd.DataFrame(index=multiindex,
-                            columns=["Number positions", "No. overdrafts", "Total overdrafts",
+                            columns=["Total margin deposited", "Number positions", "No. overdrafts", "Total overdrafts",
                                     "No. pos. on default", "Shortfall"])
     
     insolvent_positions = {}
@@ -37,7 +37,7 @@ def compute_summary_negative_statistics(identifier):
     for pool in summary_df.index:
         dataset = positions[pool[0]][pool[1]]#[["Initial balance", "rPnL", "Balance after rPnL sttlm", "uPnL", "Final balance", "Leverage"]]
                                     
-        summary_df.loc[pool] = [len(dataset), (dataset["Real balance"]<0).sum(),
+        summary_df.loc[pool] = [dataset["Initial balance"].sum(),len(dataset), (dataset["Real balance"]<0).sum(),
                             (dataset["Real balance"][dataset["Real balance"]<0]).sum(),
                             (dataset["Final balance"]<0).sum(),
                             (dataset["Final balance"][dataset["Final balance"]<0]).sum()
@@ -46,12 +46,12 @@ def compute_summary_negative_statistics(identifier):
 
         defaulted_positions[pool] = dataset[dataset["Final balance"]<0]
 
-    summary_df.to_excel(f"monitoring-data/summary-{identifier}.xlsx")
+    summary_df.to_excel(path_to_save + f"summary-{identifier}.xlsx")
 
-    with pd.ExcelWriter(f"monitoring-data/overdrafts-{identifier}.xlsx") as writer:
+    with pd.ExcelWriter(path_to_save + f"overdrafts-{identifier}.xlsx") as writer:
         for pool in insolvent_positions.keys():
             insolvent_positions[pool].to_excel(writer, sheet_name=str(pool[1]))
 
-    with pd.ExcelWriter(f"monitoring-data/defaults-{identifier}.xlsx") as writer:
+    with pd.ExcelWriter(path_to_save + f"defaults-{identifier}.xlsx") as writer:
         for pool in defaulted_positions.keys():
             defaulted_positions[pool].to_excel(writer, sheet_name=str(pool[1]))
